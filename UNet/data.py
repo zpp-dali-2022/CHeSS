b59937a8e5d16b63ba8d918277d85c93cb042556
Adrian Hess <89874503+adrihess@users.noreply.github.com>
2023-05-26 00:58:31 +0200

    Update data.py
    Added function to conversion 'npz' to 'npy' file

--- UNet/data.py
+++ UNet/data.py
@@ -157,3 +157,13 @@ def create_train_test_sets(images, masks, input_shape, normalize_images, normali
     n_test = len(test_x)
 
     return train_dataset, test_dataset, n_train, n_test
+
+# Convert '.npz' files to '.npy' format
+def convert_npz_to_npy(path):
+    npz_files = [file for file in os.listdir(path) if file.endswith('.npz')]
+    for npz_file in npz_files:
+        npz_file_path = os.path.join(path, npz_file)
+        npy_file_path = os.path.join(path, npz_file.replace('.npz', '.npy'))
+        data = np.load(npz_file_path)
+        np.save(npy_file_path, data)
+    return    
