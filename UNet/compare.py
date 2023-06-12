1dae8ba7d1647edd89ee397745a7805c373c4b4f
aderylo <a.m.derylo@gmail.com>
2023-06-12 10:49:07 +0200

    benchmarking script

--- UNet/compare.py
+++ UNet/compare.py
@@ -3,7 +3,10 @@ import time
 import glob
 import os
 import pandas as pd
+import sys
 
+# set working directory to the directory of a script
+os.chdir(os.path.dirname(sys.argv[0]))
 
 def dali_gpu_benchmark():
     start = time.time()
@@ -19,21 +22,32 @@ def fitsio_benchmark():
     return end - start
 
 
+def save_results(results : pd.DataFrame, filename : str = "benchmark_results.csv"):
+    header = False if os.path.exists(filename) else True
+    df.to_csv(filename, mode="a", header=header)
+
 if __name__ == "__main__":
+    # path to the dataset to train on
     data_path = os.getenv("DATA_PATH")
+    iterations = 10
+
     n_images = len(
         sorted(glob.glob(os.path.join(data_path, "*/fits/*.fits"), recursive=True))
     )
     results = {}
 
-    for benchmark in [dali_gpu_benchmark, fitsio_benchmark]:
-        time = benchmark()
-        results[benchmark.__name__] = {
-            "time": time,
-            "throughput": n_images / time,
-            "n_images": n_images,
-        }
+    for i in range(iterations):
+        print("=========================================================================")
+        print(f"""\n\nIteration {i}\n\n""")
+        print("=========================================================================")
+        for benchmark in [dali_gpu_benchmark(), fitsio_benchmark()]:
+            time = benchmark()
+            results[benchmark.__name__] = {
+                "time": time,
+                "throughput": n_images / time,
+                "n_images": n_images,
+            }
 
     df = pd.DataFrame(results)
-    df.to_csv("benchmark_results.csv")
     print(df)
+    save_results(df)
